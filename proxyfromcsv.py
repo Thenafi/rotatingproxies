4da38a571dfa6a04142b7a832a03021e01c915fb
Thenafi <65831153+Thenafi@users.noreply.github.com>
2022-01-13 23:37:49 +0600

    ok

--- proxyfromcsv.py
+++ proxyfromcsv.py
@@ -7,18 +7,18 @@ import concurrent.futures
 
 proxylist = []
 
-with open('proxylist.csv', 'r') as f:
+with open('gg.csv', 'r') as f:
     reader = csv.reader(f)
     for row in reader:
         proxylist.append(row[0])
-
+print(len(proxylist))
 def extract(proxy):
     #this was for when we took a list into the function, without conc futures.
     #proxy = random.choice(proxylist)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0'}
     try:
         #change the url to https://httpbin.org/ip that doesnt block anything
-        r = requests.get('https://httpbin.org/ip', headers=headers, proxies={'http' : proxy,'https': proxy}, timeout=2)
+        r = requests.get('https://api.airbnb.com/v2', headers=headers, proxies={'http' : proxy,'https': proxy}, timeout=4)
         print(proxy)
     except:
         pass
